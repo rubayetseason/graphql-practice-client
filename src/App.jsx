01569d6516f58b24cd61b2c413610d94eca10334
rubayet-islam <season.ss2001@gmail.com>
2023-02-11 23:46:52 +0600

    created a basic apollo client setup and handled error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import './App.css'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`GraphQL error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:5000/graphql" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 function App() {
-
   return (
-    <div className="App">
+    <div>
+      <h1>Practice GraphQL client part with APOLLO Client.</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
